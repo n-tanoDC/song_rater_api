3e2dcb98b07b918cf5d087f78254e4211641ce94
n-tanoDC <nicolas.tano42@gmail.com>
2020-09-25 13:00:23 +0200

    Use body parser for all routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 const userRoutes = require('./user');
 const reviewRoutes = require('./review');
 const listRoutes = require('./list');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.use('/user', userRoutes)
 router.use('/review', reviewRoutes)

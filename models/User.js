06dfa53ac9a4cd86884ea96c1cf90118a23116c6
n-tanoDC <nicolas.tano42@gmail.com>
2020-09-25 15:56:57 +0200

    Install bcrypt and hash passwords

--- models/User.js
+++ models/User.js
@@ -1,13 +1,25 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   description: String,
   password: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   favorites: [Number]
 });
+
+userSchema.pre('save', async function(next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
+
+userSchema.methods.isValidPassword = async function(password) {
+  const compare = await bcrypt.compare(password, this.password)
+  return compare;
+}
+
 
 module.exports = mongoose.model('User', userSchema);

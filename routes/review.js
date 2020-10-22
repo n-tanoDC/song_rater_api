44b44299542ca93ff61c785199464de25676fbe5
n-tanoDC <nicolas.tano42@gmail.com>
2020-10-22 09:00:39 +0200

    Add route to get paginated list of reviews for one media

--- routes/review.js
+++ routes/review.js
@@ -23,16 +23,16 @@ router.route('/')
   // post a review
   .post(authenticate, async (req, res) => {
     try {
-      const { title, rating, content, element } = req.body;
+      const { title, rating, content, media } = req.body;
       const author = await User.findById(req.user._id);
       
-      const oldReview = await Review.findOne({ author: author._id, 'element.id': element.id })
+      const oldReview = await Review.findOne({ author: author._id, 'media.id': media.id })
       
       if (oldReview) {
         throw new Error('Already exists');
       }
       
-      const review = new Review({ title, content, rating, element, author })
+      const review = new Review({ title, content, rating, media, author })
       await review.save()
       res.status(201).json(review)
     } catch (error) {
@@ -42,17 +42,23 @@ router.route('/')
     
   })
 
-router.route('/:id')
-  // get one review
+router.route('/media/:id')
+  // get all reviews for one media
   .get(async (req, res) => {
-    const { id } = req.params; 
-    const review = await Review.findById(id);
-    res.json(review);
-  })
-  .delete(async (req, res) => {
+    const { page = 1, limit = 10 } = req.query
     const { id } = req.params;
-    await Review.findByIdAndDelete(id)
-    res.send('DELETE OK')
+    const reviews = await Review.find({ 'media.id': id})
+      .populate('author')
+      .sort({ created_at: -1 })
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec()
+    const count = await Review.countDocuments()
+    // get total amount of pages possible with the limit defined in the request
+    const totalPages = Math.ceil(count / limit)
+    // generate a url for the next page if there's any page left
+    const next = page < totalPages ? (parseInt(page) + 1) : null
+    res.json({ reviews, next })
   })
 
 module.exports = router;

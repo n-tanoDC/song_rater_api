150cd9d906d742025b888982e80faa16e3120dcc
n-tanoDC <nicolas.tano42@gmail.com>
2020-10-27 12:36:30 +0100

    Install helmet

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const AdminBro = require ('./.adminbro/admin');
 const routes = require('./routes');
 
 const app = express();
+
+const hostname = 'localhost';
 const port = 8000;
 const db_host = process.env.DB_HOST;
 const db_port = process.env.DB_PORT;
 const db_name = process.env.DB_NAME;
 
-app.use(AdminBro.admin.options.rootPath, AdminBro.router)
+app.use(AdminBro.admin.options.rootPath, AdminBro.router);
+app.use(helmet());
 app.use('/', routes);
 
 mongoose.connect('mongodb://' + db_host + ':' + db_port + '/' + db_name, {
@@ -21,4 +25,4 @@ mongoose.connect('mongodb://' + db_host + ':' + db_port + '/' + db_name, {
 });
 
 const db = mongoose.connection;
-db.once('open', () => app.listen(port, () => console.log('Server started on port ' + port)));
+db.once('open', () => app.listen(port, hostname, () => console.log('Server started at ' + hostname + ':' + port)));

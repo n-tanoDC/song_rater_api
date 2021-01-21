01424621dc5ebc21132c4f063f73875bcfd6419f
n-tanoDC <nicolas.tano42@gmail.com>
2021-01-21 15:22:53 +0100

    Edit Review model to allow posting reviews without content or title

--- models/Review.js
+++ models/Review.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema ( {
-  title: { type: String, minlength: 5, maxlength: 70 },
-  content: { type: String, minlength: 10, maxlength: 5000 },
+  title: { type: String, maxlength: 70 },
+  content: { type: String, maxlength: 5000 },
   media: { type: Schema.Types.ObjectId, ref: 'Media' },
   created_at: { type: Date, default: Date.now },
   rating: { type: Number, min: 1, max: 10, required: true },
